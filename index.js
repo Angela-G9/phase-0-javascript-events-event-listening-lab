e180ad40d957dfaefe53d6ebc17cb356ae200329
Angela-G9 <angelagathoni9568@gmail.com>
2024-12-10 00:00:39 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
-}
+    const button = document.getElementById('button'); 
+    if (button) {
+      button.addEventListener('click', () => {
+        alert('I was clicked!');
+      });
+    } else {
+      console.error("Element with id 'button' not found.");
+    }
+  }
+  
